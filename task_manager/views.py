d1fde0ad58f5a3e0bed9c014650601e378445ab0
alekorn <entkorn@gmail.com>
2023-04-04 15:10:40 +0300

    init env to settings and postgres

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
+
 
 menu = ["test!", "title", "login"]
 
@@ -6,7 +8,7 @@ menu = ["test!", "title", "login"]
 def index(request):
     return render(request, 'index_test.html', context={
         "menu": menu,
-        'tst': 'TEST',
+        'tst': _('Hello World!'),
     })
 
 
